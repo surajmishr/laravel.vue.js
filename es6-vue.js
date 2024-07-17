9e11b9ad5bc2df6a8c44cb4bebd7782830538273
surajmishr <107803484+surajmishr@users.noreply.github.com>
2024-07-17 11:57:44 +0530

    Update es6-vue.js

--- es6-vue.js
+++ es6-vue.js
@@ -1,5 +1,5 @@
 //Practice
-
+https://www.youtube.com/watch?v=O-rzja-2OPk
 //template literl
 
 //const message  = 'hello\n' + 'how are you';
@@ -27,3 +27,38 @@
 // }
 
 // product.buy();
+// const = accessToken = 'dsfjchjhjd';
+// const refreshToken  =  'adkjfljdffdd';
+// const user = {
+//   accessToken : accessToken,
+//   refreshToken : refreshToken,
+// };
+// console.log(user); 
+// fir ishame inhancement aaya jo ki
+
+// distructring
+const user = {
+  name: 'john Doe',
+  agec 30
+}; 
+const {name, age} = user;
+// Agar App ko aliyash dena hai to eaily de sakte refreshToken
+const {name: fullName, age } = user;
+// console.log(fullname);
+
+const user = {
+  name: 'john Doe',
+  agec 30
+}; 
+const {name, age} = user;
+// Agar App ko aliyash dena hai to eaily de sakte refreshToken
+const {name: fullName, age } = user;
+// console.log(fullname);
+
+const data = ['Rakesh',30, 'Engineer'];
+// before console.log(data[0]);
+const [name,age,profession] = data;
+// after console.log(name);
+
+// basicly vue js aishe work karta   react 
+// const [count, SetCount] = useState()
