a9036f72cc0d7ff39f56407833c34acfe5b70249
surajmishr <107803484+surajmishr@users.noreply.github.com>
2024-07-17 11:31:38 +0530

    Create es6-vue.js

---
+++ es6-vue.js
@@ -0,0 +1,29 @@
+//Practice
+
+//template literl
+
+//const message  = 'hello\n' + 'how are you';
+
+// const name = 'rakesh';
+// const messagec =`helllo 
+// how are yoy ${name}`;
+// console.log(messagec);
+
+// Enhanced Object Literl 
+// const keyName  = 'name';
+// const product = {
+//   [keyName]: 'mobile',
+//   price: 100,
+  
+// }
+
+// console.log(product);
+
+//Methord  
+// const  product  ={
+//   buy: function(){
+//     console.log('Baught');
+//   }, 
+// }
+
+// product.buy();
