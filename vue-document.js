da3fbe6ba574cef8a865445ccc8844d3c04e8494
surajmishr <107803484+surajmishr@users.noreply.github.com>
2024-07-17 11:11:02 +0530

    Create vue-document.js

---
+++ vue-document.js
@@ -0,0 +1,9 @@
+https://youtu.be/erUHpM5lobY?si=hbEWZuNu2kV1eyE-
+Vue.js 
+progressive Framework 
+The Word 
+Routing 
+Scalability 
+Ready to use Conventions
+Developers Choice
+Server Side Rendering 
